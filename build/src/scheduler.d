db1fee03f88fddfbf6b8def79328a88be6153be7
Dimitrije1970 <dimitrije.ilic1970@gmail.com>
2023-09-14 10:13:47 -0700

    Gotov projekat.

--- build/src/scheduler.d
+++
@@ -1,8 +0,0 @@
-build/src/scheduler.o: src/scheduler.cpp src/../h/scheduler.hpp \
- src/../h/MemoryAllocator.hpp src/../h/../lib/hw.h
-
-src/../h/scheduler.hpp:
-
-src/../h/MemoryAllocator.hpp:
-
-src/../h/../lib/hw.h:
--- build/src/sycall_c.d
+++
@@ -1,8 +0,0 @@
-build/src/sycall_c.o: src/sycall_c.cpp src/../h/syscall_c.h \
- src/../h/../lib/hw.h src/../lib/console.h
-
-src/../h/syscall_c.h:
-
-src/../h/../lib/hw.h:
-
-src/../lib/console.h:
